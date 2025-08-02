11a2b6f5ac86a036664a7107f77a8049e8013fcc
HasibulHRony <hasibulhrony7@gmail.com>
2025-08-02 11:45:55 +0600

    some if else practices

---
+++ if_else/if-else.js
@@ -0,0 +1,90 @@
+if(3 < 10){
+    console.log("I am small. I do not bite;")
+}
+
+if(13 < 10){
+    console.log("Smaller but stronger")
+}
+
+let biryaniPrice = 500;
+if(biryaniPrice < 300){
+    console.log("Mama, Give me some biryani");
+}
+
+
+const rainingOutside = true;
+if(rainingOutside == true){
+    console.log("You and me, Under the tree;")
+}
+else(
+    console.log("No rain, No romance.")
+)
+
+const weight = 30;
+if(weight < 40){
+    console.log("Rickshaw mama cholo jai.")
+}
+else{
+    console.log("Walking is Exercising!")
+}
+
+
+/**
+ * practice
+ */
+
+//practice 1
+let returnHome = 6;
+if (returnHome <= 6 === true){
+    console.log("Nasta Kha!")
+} 
+else{
+    console.log("Jhatar bari kha!")
+}
+
+// practice 2
+let logIn = true;
+if(logIn === true){
+    console.log("You are welcome.")
+}
+else{
+    console.log("Get Lost.")
+}
+
+
+//practice 3
+let runningKM = 4.6;
+if(runningKM > 5 === true){
+    console.log("Tor vuri hoye jabe!")
+}
+else{
+    console.log("Wow! Ne, Chocolate kha!")
+}
+
+// practice 4
+let marks = 85;
+if(marks > 80){
+    console.log("Wow! sonamoni, nao bike nao!")
+}
+else{
+    console.log("Bari theke cole ja!")
+}
+
+
+// practice 5
+let movieShaw = 9.30;
+if(movieShaw < 9){
+    console.log("I will see the shaw!")
+}
+else{
+    console.log("Nake tel dia ghumabo!")
+}
+
+//practice 6
+let temp = 19;
+if(temp <= 30){
+    console.log("Kombol muri de!")
+}
+else{
+    console.log("On the AC")
+}
---
+++ if_else/more_if_else.js
@@ -0,0 +1,150 @@
+const salary = 75000;
+if(salary > 50000){
+    console.log("supatro!")
+}
+else{
+    console.log("next 5year single thako")
+}
+
+
+//and condition
+// const beton = 64000;
+// const isBcs = true;
+// if(beton >= 50000 && isBcs === true){
+//     console.log("sei leveler patro!")
+// }
+// else{
+//     console.log("Dure giya mor!")
+// }
+//output: sei leveler patro!
+
+
+// const beton = 64000;
+// const isBcs = false;
+// if(beton >= 50000 && isBcs === true){
+//     console.log("sei leveler patro!")
+// }
+// else{
+//     console.log("Dure giya mor!")
+// }
+//output: dure giya mor
+
+
+// const salary2 = 25000;
+// const height = 68;
+// if(salary2 > 25000 || height){
+//     console.log("Bolo baba kobul")
+// } 
+// else{
+//     console.log("dure giye vag mokbul!")
+// }
+
+
+// const salary2 = 55000;
+// const height = 68;
+// if(salary2 > 25000 || height){
+//     console.log("Bolo baba kobul")
+// } 
+// else{
+//     console.log("dure giye vag mokbul!")
+// }
+
+
+
+/**
+ * practices
+ */
+
+//practice 1
+let age = 17;
+let height = 66;
+if(age >= 18 && height > 60){
+    console.log("Gari Thela de!")
+}
+else{
+    console.log("Bose bose ja!")
+}
+
+
+//practice 2
+let mathMark = 84;
+let englishMark = 79;
+if(mathMark > 80 || englishMark > 85){
+    console.log("Tomar admission confirm!")
+}
+else{
+    console.log("Tomake biye dia deoa hobe!")
+}
+
+
+//practice 3
+let result = 5;
+let income = 9000;
+if(result >= 5 && income < 10000){
+    console.log("Britti confirm!")
+}
+else{
+    console.log("Beton dite hobe!")
+}
+
+
+//practice 4
+let candidateAge = 28;
+let experience = 2;
+if(candidateAge < 30 && experience >= 2){
+    console.log("Exam dite eso!")
+}
+else{
+    console.log("Sorry!")
+}
+
+//practice 5
+let egg = 14;
+let isChicken = false;
+if(egg > 12 && isChicken === false){
+    console.log("Cooking dimer kurma!")
+}
+else{
+    console.log("Pauruti ar kola kha!")
+}
+
+// practice 6
+let tem = 97.4;
+let cough = false;
+if(tem >= 100 || cough === true){
+    console.log("Doctorer kache ja!")
+}
+else{
+    console.log("Katha muri diye suye thak!")
+}
+
+
+//practice 7
+let presence = 89;
+let homeWork = true;
+if(presence > 80 && homeWork === true){
+    console.log("You can attend to exam!")
+}
+else{
+    console.log("Auto fail.")
+}
+
+//practice 8
+let isElectricity = true;
+let isCharged = false;
+if(isElectricity === true && isCharged === false){
+    console.log("porte bosbo")
+}
+else{
+    console.log("video dekhbo")
+}
+
+//practice 9
+let price = 1200;
+let isCuppon = true;
+if(price > 1000 && isCuppon === true){
+    console.log("20% discount")
+}
+else{
+    console.log("no discount. pay full amount")
+}
