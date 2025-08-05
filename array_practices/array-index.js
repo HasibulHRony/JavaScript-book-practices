57d2d532f84ef42171aabb0d8212d9f0314ec339
HasibulHRony <hasibulhrony7@gmail.com>
2025-08-05 16:49:35 +0600

    array basics

---
+++ array_practices/array-index.js
@@ -0,0 +1,46 @@
+const numbers = [10, 25, 26, 31, 42, 84, 38, 12, 1];
+const fourth = numbers[4];
+console.log(fourth);
+
+
+const fourteen = numbers[14];
+console.log(fourteen)
+
+
+numbers[4] = 999999;
+console.log(numbers)
+
+console.log(fourth)
+
+
+
+//practices
+// practice1
+const friends = ["asif", "maruf", "mahfuz", "siam"];
+console.log(friends[3]);
+
+//practice2
+const favoriteBooks = ["physics", "chemistry", "biology", "math", "history", "philosophy", "political science"];
+console.log(favoriteBooks[4]);
+
+//practice3
+const counts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+counts[6] = 30;
+console.log(counts[6]);
+
+//practice4
+const games = ["freefire", "pubge", "cudycrush", "templerun"];
+games[2] = "subwaysurfar";
+console.log(games[2]);
+
+//practice5
+const families = ["ZindarAli", "FaridulIslam", "HosnayaraBegum", "HasibulHasan"];
+console.log(families[2]);
+
+//practices6
+const countries = ["Bangladesh", "Nepal", "China", "Russia", "Pakistan", "UAE", "Turkey"];
+console.log(countries[3]);
+
+//practice7
+const studyMatrials = ["Books", "Pens", "Papers", "Markers"];
+console.log(studyMatrials[6])
---
+++ array_practices/array_basic.js
@@ -0,0 +1,44 @@
+const marks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const friends = ["Aboul", "Babul", "Cabul", "Dabul", "Bulbul"];
+const passed = [true, false, true, false];
+const mixedArray = [12, "dim", true, 45.556];
+
+const numbers = [1, 5, 66, 11, 45];
+const size = numbers.length;
+console.log(size)
+
+/**
+ * Practices
+ */
+
+// practice 1
+const array = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+
+// practice 2
+const fruits = ["Apple", "Banana", "Orange", "Mango", "Guava"];
+
+// practice 3
+let vegetable = ["alu", "morich", "tomato", "begun", "potol", "lau"];
+let total = vegetable.length;
+console.log(total);
+
+//practice 4
+let fevMovie = ["The Shashank Redemption", "3Idiots", "PK", "Jawaan"];
+
+//practice 5
+let oddNums = [];
+for(let i = 11; i <=30; i++){
+    if(i % 2 === 1){
+        oddNums.push(i);
+    }
+}
+console.log(oddNums);
+
+//practice 6
+let colors = ["green", "red", "blue", "orange"]
+let colorsNum = colors.length;
+console.log(colorsNum)
+
+//practice 7
+let capitals = ["Dhaka", "Islamabad", "Tehran", "Istanbul", "Sana"];
+console.log(capitals.length)
